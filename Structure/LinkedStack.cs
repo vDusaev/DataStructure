26908440943eed893934cb324286ec41b662d510
Vladimir <vdusaev@gmail.com>
2020-12-21 17:39:49 +0300

    Add linked stack

---
+++ Structure/LinkedStack.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Structure
+{
+    public class LinkedStack<T>
+    {
+        public LinkedStackItem<T> Head { get; set; } = null;
+        public int Count { get; set; } = 0;
+
+        public LinkedStack() { }
+        public LinkedStack(T data) 
+        {
+            Head = new LinkedStackItem<T>(data);
+            Count = 1;
+        }
+
+        public void Push(T data)
+        {
+            var newItem = new LinkedStackItem<T>(data);
+            newItem.Previous = Head;
+            Head = newItem;
+            Count++;
+        }
+        public T Pop()
+        {
+            if (Count > 0)
+            {
+                var currentHead = Head;
+                Head = currentHead.Previous;
+                Count--;
+
+                return currentHead.Data;
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+        public T Peek()
+        {
+            if (Count > 0)
+            {
+                return Head.Data;
+            }
+            else
+            {
+                throw new NullReferenceException("Stack is empty");
+            }
+        }
+    }
+}
---
+++ Structure/LinkedStackItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Structure
+{
+    public class LinkedStackItem<T>
+    {
+        private T data = default(T);
+        public T Data
+        {
+            get => data;
+            set
+            {
+                if (value != null)
+                    data = value;
+                else
+                    throw new ArgumentNullException(nameof(value));
+            }
+        }
+        public LinkedStackItem<T> Previous { get; set; }
+
+        public LinkedStackItem(T data)
+        {
+            Data = data;
+        }
+
+        public override string ToString()
+        {
+            return Data.ToString();
+        }
+    }
+}
