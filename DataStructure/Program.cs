e153cd500a6e2512377f6d1f975e84474a697d64
Vladimir <vdusaev@gmail.com>
2021-01-04 19:55:28 +0300

    Add linked queue

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -7,7 +7,7 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-
+            
         }
     }
 }
---
+++ Structure/LinkedQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Structure
+{
+    public class LinkedQueue<T>
+    {
+        private ItemDuplex<T> head;
+        private ItemDuplex<T> tail;
+        public int Count { get; private set; }
+
+        public LinkedQueue() { }
+        public LinkedQueue(T data)
+        {
+            SetNewHeadAndTail(data);
+        }
+
+        public void Enqueue(T data)
+        {
+            if (Count == 0)
+            {
+                SetNewHeadAndTail(data);
+                return;
+            }
+
+            var item = new ItemDuplex<T>(data);
+            tail.Previous = item;
+            tail = item;
+            Count++;
+        }
+        public T Dequeue()
+        {
+            if (Count == 0)
+            {
+                throw new ArgumentNullException("Queue is null");
+            }
+
+            var item = head;
+            head.Previous.Next = null;
+            head = item.Previous;
+            return item.Data;
+        }
+        public T Peek()
+        {
+            return head.Data;
+        }
+
+        private void SetNewHeadAndTail(T data)
+        {
+            head = new ItemDuplex<T>(data);
+            tail = head;
+            Count = 1;
+        }
+    }
+}
