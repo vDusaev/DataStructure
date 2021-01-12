22c3b3ab8d45503174078ea74a9a887b21e4c940
Vladimir <vdusaev@gmail.com>
2021-01-12 20:01:36 +0300

    Add linked hash table

---
+++ Structure/ItemHashTable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace Structure
+{
+    public class ItemHashTable<T>
+    {
+        public int Key { get; set; }
+        public List<T> Nodes { get; set; }
+
+        public ItemHashTable(int key)
+        {
+            Key = key;
+            Nodes = new List<T>();
+        }
+    }
+}
---
+++ Structure/LinkedHashTable.cs
@@ -0,0 +1,32 @@
+﻿namespace Structure
+{
+    public class LinkedHashTable<T>
+    {
+        private ItemHashTable<T>[] items;
+
+        public LinkedHashTable(int size = 10)
+        {
+            items = new ItemHashTable<T>[size];
+            for (int i = 0; i < size; i++)
+            {
+                items[i] = new ItemHashTable<T>(i);
+            }
+        }
+
+        public void Add(T item)
+        {
+            var key = getHashCode(item);
+            items[key].Nodes.Add(item);
+        }
+        public bool Search(T item)
+        {
+            var key = getHashCode(item);
+            return items[key].Nodes.Contains(item);
+        }
+
+        private int getHashCode(T item)
+        {
+            return item.GetHashCode() % items.Length;
+        }
+    }
+}
