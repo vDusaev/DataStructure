39024738532b9d67088b0fd20157c31f89617bbd
Vladimir <vdusaev@gmail.com>
2021-01-14 17:49:15 +0300

    Add linked dictionary

--- DataStructure/Program.cs
+++ DataStructure/Program.cs
@@ -7,7 +7,30 @@ namespace DataStructure
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Linked dictionary:");
+
+            var dictionary = new LinkedDictionary<int, string>();
+            dictionary.Add(1, "One");
+            dictionary.Add(2, "Two");
+            dictionary.Add(3, "Three");
+            dictionary.Add(101, "One hundred one");
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine("\nRemove 1, 101, 5: ");
+            dictionary.Remove(1);
+            dictionary.Remove(101);
+            dictionary.Remove(5);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine($"\nSearch 2: {dictionary.Search(2)}");
         }
     }
 }
---
+++ Structure/LinkedDictionary.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Structure
+{
+    public class LinkedDictionary<TKey, TValue> : IEnumerable
+    {
+        private ItemMap<TKey, TValue>[] items;
+        private List<TKey> keys = new List<TKey>();
+        private int size;
+
+        public LinkedDictionary(int size = 100)
+        {
+            this.size = size;
+            items = new ItemMap<TKey, TValue>[this.size];
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (keys.Contains(key))
+            {
+                return;
+            }
+
+            var hashCode = getHash(key);
+
+            var flag = false;
+            for (int i = hashCode; i < size; i++)
+            {
+                if (items[i] == null)
+                {
+                    items[i] = new ItemMap<TKey, TValue>(key, value);
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                for (int i = 0; i < hashCode; i++)
+                {
+                    if (items[hashCode] == null)
+                    {
+                        items[hashCode] = new ItemMap<TKey, TValue>(key, value);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public void Remove(TKey key)
+        {
+            if (keys.Contains(key))
+            {
+                return;
+            }
+
+            var hashCode = getHash(key);
+
+            var flag = false;
+            for (int i = hashCode; i < size; i++)
+            {
+                if (items[i] != null && items[i].Key.Equals(key))
+                {
+                    items[i] = null;
+                    flag = true;
+                    break;
+                }
+            }
+            if (!flag)
+            {
+                for (int i = 0; i < hashCode; i++)
+                {
+                    if (items[i] != null && items[i].Key.Equals(key))
+                    {
+                        items[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+
+        public TValue Search(TKey key)
+        {
+            if (keys.Contains(key))
+            {
+                return default(TValue);
+            }
+
+            var hashCode = getHash(key);
+
+            var flag = false;
+            for (int i = hashCode; i < size; i++)
+            {
+                if (items[i] != null && items[i].Key.Equals(key))
+                {
+                    return items[i].Value;
+                }
+            }
+            if (!flag)
+            {
+                for (int i = 0; i < hashCode; i++)
+                {
+                    if (items[i] != null && items[i].Key.Equals(key))
+                    {
+                        return items[i].Value;
+                    }
+                }
+            }
+
+            return default(TValue);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            foreach (var item in items)
+            {
+                if (item != null)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        private int getHash(TKey key)
+        {
+            return key.GetHashCode() % size;
+        }
+    }
+}
